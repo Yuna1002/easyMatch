e70da61f33d035ea1e885454d0e7e272229fac64
Yuna1002 <e2431666@gmail.com>
2023-12-05 22:11:17 +0800

    deploy

--- assets/scrollStore-ba1954a8.js
+++
@@ -1,1 +0,0 @@
-import{E as o}from"./index-ffca9aed.js";const r=o("scroll",{state:()=>({scrollPosition:0}),actions:{scrollToTop(){window.scrollTo({top:this.scrollPosition,behavior:"smooth"})}}});export{r as s};
